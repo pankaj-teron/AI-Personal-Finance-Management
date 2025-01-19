731b7c4d80dbdd4ff329e9410e14fc5a453c7b5d
Pankaj Teron <pnkj123teron@gmail.com>
2025-01-20 02:43:11 +0530

    components created

---
+++ components/header.jsx
@@ -0,0 +1,19 @@
+// "rafc" boiler code "reactArrowFunctionComponent"
+// "rafce" boiler code "reactArrowFunctionExportComponent"
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
+const Header = () => {
+  return (
+    <div className="fixed top-0">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
